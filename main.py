052b140161d75a17ea90b5589f929f48fe0639bf
Salvador Guzman <guzmansalv@gmail.com>
2018-05-26 21:14:49 -0700

    fixed up

---
+++ main.py
@@ -0,0 +1,12 @@
+import brotli
+import sys
+import os
+
+filename = sys.argv[1]
+file = open(filename, 'rb').read()
+links = brotli.decompress(file)
+links_split = links.split(b'\n')[:-1]
+for i in links_split:
+    out_file = f'./{i}.html.brotli'
+    if os.path.exists(out_file) is False:
+        print(i)
